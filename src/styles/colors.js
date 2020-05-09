3a08c55490cd189bdb2a725f1b262af42446197c
Sebastian Perez <sperezm97@gmail.com>
2020-05-09 02:12:55 -0400

    Add colors

---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+export default {
+  primaryColor: '#0072FF',
+  primaryColorDark: '#4C47F4',
+  primaryColorLight: '#008AFF',
+  secondaryColor: '#00AA7D',
+  secondaryColorDark: '#00A3B8',
+  secondaryColorLight: '',
+  accentColor: '',
+  grey: '#f4f4f4',
+  greyLight: '',
+  greyDark: '',
+
+  textColor: '#333333',
+
+  danger: '#cc0000',
+  success: '#ff8800',
+  caution: '#007e33',
+
+  white: '#fff',
+  black: '#000',
+};
